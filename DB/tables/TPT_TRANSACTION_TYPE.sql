53b4cc4de98fe074457e3eb950e38401db64e356
Anfel-l <anfellr11@gmail.com>
2023-09-25 20:09:42 -0500

    DB: transaction_type_id was added to document_type table

--- DB/tables/TPT_TRANSACTION_TYPE.sql
+++ DB/tables/TPT_TRANSACTION_TYPE.sql
@@ -7,6 +7,7 @@ Management Id: XD01
 *******************************************************************************/
 
 CREATE TABLE IF NOT EXISTS USER.TRANSACTION_TYPE(
+    transaction_id NUMBER(10) NOT NULL,
     transaction_type_code VARCHAR2(3) PRIMARY KEY,
     description VARCHAR2(50)
 )
@@ -18,6 +19,8 @@ INSERT INTO USER.TRANSACTION_TYPE (transaction_type_code, description) VALUES ('
 INSERT INTO USER.TRANSACTION_TYPE (transaction_type_code, description) VALUES ('3', 'Interéses')
 /
 COMMENT ON TABLE USER.TRANSACTION_TYPE IS 'Document type table'
+/
+COMMENT ON COLUMN USER.TRANSACTION_TYPE.transaction_id IS 'Unique identifier for the transaction type. It is the primary key for the table.'
 /
 COMMENT ON COLUMN USER.TRANSACTION_TYPE.transaction_type_code IS 'Unique code representing the type of transaction. It is the primary key for the table.'
 /
