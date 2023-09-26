54c825efab6f1b185cc1d8c6bb5809816f4abc1f
Anfel-l <anfellr11@gmail.com>
2023-09-25 20:06:08 -0500

    DB: document_type_id was added to document_type table

--- DB/tables/TPT_DOCUMENT_TYPE.sql
+++ DB/tables/TPT_DOCUMENT_TYPE.sql
@@ -7,12 +7,14 @@ Management_id: XD01
 *******************************************************************************/
 
 CREATE TABLE IF NOT EXISTS USER.DOCUMENT_TYPE(
+    document_type_id NUMBER(10),
     type_code VARCHAR2(3) PRIMARY KEY,
     description VARCHAR2(50)
 )
 /
 COMMENT ON TABLE USER.DOCUMENT_TYPE IS 'Table that stores the different types of documents that a customer can have.'
 /
+COMMENT ON COLUMN USER.DOCUMENT_TYPE.document_type_id IS 'Unique identifier for the document type. It is the primary key for the table.'
 COMMENT ON COLUMN USER.DOCUMENT_TYPE.type_code IS 'Unique code representing the type of document. It is the primary key for the table.'
 /
 COMMENT ON COLUMN USER.DOCUMENT_TYPE.description IS 'Description of the document type, explaining what each code represents.'
