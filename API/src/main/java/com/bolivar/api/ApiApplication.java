99241c8d5fdd45a6f066e3c697755d3d25fca7fc
David Fajardo <nidafa202426@gmail.com>
2023-09-27 14:55:32 -0500

    fix: config project

--- API/src/main/java/com/bolivar/api/ApiApplication.java
+++ API/src/main/java/com/bolivar/api/ApiApplication.java
@@ -1,13 +1,13 @@
-package com.segurosbolivar.taller;
+package com.bolivar.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TallerApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TallerApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
--- API/src/test/java/com/bolivar/api/ApiApplicationTests.java
+++ API/src/test/java/com/bolivar/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.segurosbolivar.taller;
+package com.bolivar.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TallerApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
