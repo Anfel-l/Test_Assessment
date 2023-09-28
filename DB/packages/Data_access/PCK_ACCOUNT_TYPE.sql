ebe0c25843a6b001955410fd0b9bb21b95c0d67b
Erika Yessenia Restrepo Sandoval <erikarestrepos@MacBook-Air-de-Erika.local>
2023-09-28 10:56:22 -0500

    Terminar package ACCOUNT_TYPE

--- DB/packages/Data_access/PCK_ACCOUNT_TYPE.sql
+++ DB/packages/Data_access/PCK_ACCOUNT_TYPE.sql
@@ -1,3 +1,4 @@
+
 CREATE OR REPLACE PACKAGE PCK_ACCOUNT_TYPE IS
 /*******************************************************************************
 Description: Table that stores information about bank's transactions
@@ -21,26 +22,61 @@ Management Id: XD01
 
     PROCEDURE insACCOUNTTYPE (accountType IN OUT NOCOPY tyrcACCOUNTTYPE);
 
+    PROCEDURE getACCOUNTTYPE (id in NUMBER, objACCOUNTTYPE out nocopy tyrcACCOUNTTYPE);
+
 END PCK_ACCOUNT_TYPE;
+
+
 
 CREATE OR REPLACE PACKAGE BODY PCK_ACCOUNT_TYPE IS
 
     /*Insert an account type*/
-    PROCEDURE insACCOUNTTYPE(accountType IN OUT NOCOPY tyrcACCOUNTTYPE) is 
+    PROCEDURE insACCOUNTTYPE(objAccountType IN OUT NOCOPY tyrcACCOUNTTYPE) is
+
+        ACCOUNT_TYPE_EXCEPTION EXCEPTION; 
+
     BEGIN
 
         -- Initialize values
-        accountType.account_type_id := SEQ.NECTVAL;
+        objAccountType.account_type_id := SEQ_ACCOUNT_TYPE.NEXTVAL;
 
         -- Insert register
-        INSERT INTO ACCOUNT_TYPE accountType;
+        INSERT INTO ACCOUNT_TYPE VALUES /*+PCK_ACCOUNT_TYPE.insACCOUNTTYPE*/ objAccountType;
 
-        -- Trow Exception
+    -- Trow Exception
     EXCEPTION
         WHEN DUP_VAL_ON_INDEX THEN
-            DBMS_OUTPUT.PUT_LINE('Error: Valor duplicado en la clave primaria o única.');
-        WHEN 
+            DBMS_OUTPUT.PUT_LINE('Error: Valor duplicado en la clave primaria o única [PCK_ACCOUNT_TYPE.insACCOUNTTYPE]');
+        WHEN ACCOUNT_TYPE_EXCEPTION THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Error creating ACCOUNT_TYPE [PCK_ACCOUNT_TYPE.insACCOUNTTYPE]');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, SQLCODE || ' => ' || SQLERRM);
         
-    END;
+    END insACCOUNTTYPE;
+    
+    
+    PROCEDURE getACCOUNTTYPE (id in NUMBER, objACCOUNTTYPE out nocopy tyrcACCOUNTTYPE) IS
+
+        ACCOUNT_TYPE_EXCEPTION EXCEPTION; 
+
+        -- Indicar columnas
+        CURSOR curACCOUNTTYPE IS
+            SELECT * 
+            FROM ACCOUNT_TYPE
+            WHERE /*+PCK_ACCOUNT_TYPE.getACCOUNTTYPE*/ account_type_id = id;
+
+    BEGIN
+        OPEN curACCOUNTTYPE;
+        FETCH curACCOUNTTYPE INTO objACCOUNTTYPE;
+        CLOSE curACCOUNTTYPE;
+    
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('Error: No hay ningun resultado [PCK_ACCOUNT_TYPE.getACCOUNTTYPE]');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, SQLCODE || ' => ' || SQLERRM);
+    END getACCOUNTTYPE;
+
+
 END;
 
